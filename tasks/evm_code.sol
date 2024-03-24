7b0a0215d6881cec5268bc49b63f47e5de7e9d0c
chana0 <82120133+chana0@users.noreply.github.com>
2024-03-24 23:29:00 +0200

    Update evm_code.sol

--- tasks/evm_code.sol
+++ tasks/evm_code.sol
@@ -1,7 +1,15 @@
-ADD
-POP 
-LT
-PUP1
-PUSH1
-PUSH2
-PUP2
+contact Example{
+    function foo()
+    {
+        assemply{
+           /*2*/
+            PUSH1 8
+            PUSH1 9
+            PUSH1 5
+            PUSH2 3
+            PUSH2 4
+            POP
+        }
+    }
+}
+
