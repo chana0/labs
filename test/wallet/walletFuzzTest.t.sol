465fa73431a3cb472eb5ec5731513c8ffb57183d
chana0 <82120133+chana0@users.noreply.github.com>
2024-03-31 00:08:54 +0200

    Update walletFuzzTest.t.sol

--- test/wallet/walletFuzzTest.t.sol
+++ test/wallet/walletFuzzTest.t.sol
@@ -31,6 +31,26 @@ contract WalletTest is Test {
         wallet.whithdraw(sum);
         assertEq(wallet.getBalance(),balances-sum);
     }
+ function testAddOwner(address ad)public{
+        wallet.addOwner(address(ad));
+        assertEq(wallet.count(),1);
+        assertEq(wallet.owners(address(ad)),true);
+    }
+    function testAddOwnersAlreadyExist(address z)public{
+        wallet.addOwner(address(z));
+        assertEq(wallet.count(),1);
+        assertEq(wallet.owners(address(z)),true);
+        vm.expectRevert('The owner already exist');
+        wallet.addOwner(address(z));
+    }
+    function testHaveTreeOwners(address ad)public{
+        w.addOwner(vm.addr(1));
+        w.addOwner(vm.addr(2));
+        w.addOwner(vm.addr(3));
+        assertEq(w.count(),3);
+        vm.expectRevert('Tree are already 3 owners');
+        w.addOwner(address(ad));
+    }
     function testAddOwner(address owner)
     { }
     function testAddMoreThanTreeOwners(address owner)public{
