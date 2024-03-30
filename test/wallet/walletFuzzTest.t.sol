7aa494e312f39ea1c828e9d57ed4818e74854ec7
chana0 <82120133+chana0@users.noreply.github.com>
2024-03-30 22:24:46 +0200

    Update walletFuzzTest.t.sol

--- test/wallet/walletFuzzTest.t.sol
+++ test/wallet/walletFuzzTest.t.sol
@@ -13,6 +13,24 @@ contract WalletTest is Test {
     function setUp() public {
         w = new Wallet();
     }
+    function testDeposite(uint sum)public{
+        uint initialBalance=wallet.getBalance();
+        address ad=vm.addr(20);
+        vm.startPrank(ad);
+        vm.deal(ad,sum);
+        payable(wallet).transfer(sum);
+        assertEq(wallet.getBalance(),initialBalance+sum,'Balance should increase by nun after transfer');
+        vm.startPrank();
+    }
+    function fuzzTestWhithdraw(uint sum)payable public{
+        payable(wallet).transfer(sum);
+        uint Balances=wallet.getBalance();
+        address ad=vm.addr(12);
+        wallet.addOwner(ad);
+        vm.prank(ad);
+        wallet.whithdraw(sum);
+        assertEq(wallet.getBalance(),balances-sum);
+    }
     function testAddOwner(address owner)
     { }
     function testAddMoreThanTreeOwners(address owner)public{
