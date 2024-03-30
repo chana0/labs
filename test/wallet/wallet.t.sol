5ded73dc0c187ce61a4fc760d897be07d7d803c9
chana0 <82120133+chana0@users.noreply.github.com>
2024-03-30 21:25:00 +0200

    Update wallet.t.sol

--- test/wallet/wallet.t.sol
+++ test/wallet/wallet.t.sol
@@ -13,10 +13,53 @@ contract WalletTest is Test {
     function setUp() public {
         w = new Wallet();
     }
-
+    function testAddOwner public()
+    {
+        w.addOwner(address(1));
+        assertEq(w.count(),1);
+        assertEq(w.owners(address(1)),true);
+    }
+    function testAddMoreThanTreeOwners()public{
+        w.addOwner(address(1));
+        w.addOwner(address(2));
+        w.addOwner(address(3));
+        vm.expectRevert('There are already 3 owners cant add owner');
+        w.addOwner(address(4));
+    }
+    function testDeleteNotExsistUser()public{
+        address owner=vm.addr(1);
+        w.addOwner(owner);
+        assertEq(w.owners(owner),true);
+        assertEq(w.count(),1);
+        w.deleteOwner(owner);
+        assertEq(w.owners(owner),false);
+        assertEq(w.count(),0);
+    }
+    function testDeleteNotExsistUser()public{
+        vm.expectRevert('Owner not exsits');
+        w.deleteOwner(vm.addr(1));
+    }
     function testDeposit() public {
+        uint balance=w.getBalance();
+        uint sum=10000000;
+        payable(address(w)).transfer(sum);
+        assertEq(w.getBalance(),sum+balance);
     }
 
     function testWithdraw() public {
+        uint sum=100;
+        uint balance=150;
+        payable(address(w)).transfer(balance);
+        w.addOwner(vm.addr(1));
+        vm.prank(vm.addr(1));
+        w.withdraw(sum);
+        assertEq(w.getBalance(),balance-sum);
+    }
+    function testWithdrawithoutEnoughMoney()public{
+        uint sum=50;
+        uint balance=10;
+        payable(w).transfer(balance);
+        vm.expectRevert();
+        w.withdraw(sum);
     }
 }
