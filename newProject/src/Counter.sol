0d716c4a694b49ad2b8ea8d66fb9f876b4bfe3a1
chana0 <chana01021@gmail.com>
2024-04-07 13:20:10 +0300

    fff

--- newProject/src/Counter.sol
+++ newProject/src/Counter.sol
@@ -1,14 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        
     }
 }
